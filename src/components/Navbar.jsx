ddab1c1b9990898a429e9fa8332eb1845942480d
riley hsiao <hsiao56@purdue.edu>
2024-05-17 19:10:29 -0400

    experience

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navigation = React.forwardRef((props, ref) => {
               About
             </NavLink>
           )}
-             {experiences.show && (
+          {experiences.show && (
             <NavLink
               className="nav-item lead"
               href={process.env.PUBLIC_URL + "/#experiences"}
