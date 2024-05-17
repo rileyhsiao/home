848ebeac9ddfed85aceb12a12145a4f70d9d54c3
riley hsiao <hsiao56@purdue.edu>
2024-05-17 18:20:54 -0400

    pic

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -48,7 +48,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: "../editable-stuff/IMG_1315.JPG",
+  imageLink: "../editable-stuff/IMG_1315.png",
   imageSize: 375,
   message:
     "My name is Chu-Yun (Riley) Hsiao. I’m a graduate of 2024 from Purdue University with a Master's degree in Business Analyst and Information Management. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of Data analyst. In my free time I like working on open source projects.",
@@ -76,12 +76,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../editable-stuff/IMG_1315.png"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../editable-stuff/IMG_1315.png"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
