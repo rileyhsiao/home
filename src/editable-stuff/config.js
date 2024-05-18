4628659ed4b5af1a1830f446eda0c50c79466acf
riley hsiao <hsiao56@purdue.edu>
2024-05-17 20:52:02 -0400

    cmpetition

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -62,9 +62,9 @@ const repos = {
 };
 
 // Leadership SECTION
-const leadership = {
+const competition = {
   show: true,
-  heading: "Leadership",
+  heading: "Competition",
   message:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
@@ -142,4 +142,4 @@ const experiences = {
 //   show: true,
 // };
 
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export { navBar, mainBody, about, repos, skills, competition, getInTouch, experiences };
